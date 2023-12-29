639bb241b534e2d34cdc7af358362fbf9786b5af
anakhapavi <71835996+anakhapavi@users.noreply.github.com>
2023-12-29 16:18:52 +0530

    Advanced Language Features

---
+++ AnonymousFunction/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    public delegate int sumDelegate(int x,int y);
+    static void Main(string[] args) 
+    {
+        sumDelegate d1 = delegate(int x,int y) {return x+y;};
+        Console.WriteLine(d1(100,200));
+    }
+}
