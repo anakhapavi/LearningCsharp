bae601dca6fc1995b20ca68072cdb4d552db2896
anakhapavi <71835996+anakhapavi@users.noreply.github.com>
2023-12-29 16:27:25 +0530

    Asynchronous Programming

---
+++ PLINQ/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] numbers = Enumerable.Range(1,20).ToArray();
+//Regular LINQ
+var query = from num in numbers 
+            where num%2 == 0
+            select num;
+System.Console.WriteLine("Regualr LIN query");
+foreach(var num in query)
+    System.Console.WriteLine(num);
+
+//PLINQ
+var parallelQuery = from num in numbers.AsParallel().AsOrdered()
+                    where num%2 == 0
+                    select num;
+System.Console.WriteLine("PLINQ query");
+foreach(var item in parallelQuery)
+    System.Console.WriteLine(item);
+
+//PLINQ with WithDegreeOfParallelism
+var query2 = numbers.AsParallel()
+             .WithDegreeOfParallelism(3)
+             .Where(num=> num%2 == 0)
+             .Select(num=> num);
+System.Console.WriteLine("PLINQ - WithDegreeOfParallelism");
+foreach(var item in query2)
+    System.Console.WriteLine(item);
---
+++ Task/Program.cs
@@ -0,0 +1,26 @@
+﻿static void Method1(int id, int sleepTime)
+{
+    System.Console.WriteLine("Task {0} is beggining",id);
+    Thread.Sleep(sleepTime);
+    System.Console.WriteLine("Task {0} is completed",id);
+}
+static void Method2(int id, int sleepTime)
+{
+    System.Console.WriteLine("Task {0} is beggining",id);
+    Thread.Sleep(sleepTime);
+    System.Console.WriteLine("Task {0} is completed",id);
+}
+var t1 = new Task(()=>Method1(1,1500));
+/*
+t1.Start();
+var t2 = new Task(()=>Method1(2,3000));
+t2.Start();
+*/
+
+//Task.Factory.StartNew
+var t3 = Task.Factory.StartNew(()=>Method1(3,1000)).ContinueWith((prevTask)=>Method2(1,1000));
+//Task.WaitAll(t2,t1);
+
+
+System.Console.WriteLine("Press any key to quit");
+Console.ReadKey();
